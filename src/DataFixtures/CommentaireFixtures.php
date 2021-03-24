c206ea3062b6327d5922c5af68db13cfed25a88f
Skygal44 <58173147+Skygal44@users.noreply.github.com>
2021-03-24 01:40:42 +0100

    Debug des fixtures

--- src/DataFixtures/CommentaireFixtures.php
+++ src/DataFixtures/CommentaireFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Commentaire;
+use App\Entity\Event;
 use App\Form\EventType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -20,31 +21,31 @@ class CommentaireFixtures extends Fixture
         $comment1=new Commentaire();
         $date2 = "22-04-2022";
         $roles[]='ROLE_ADMIN';
-        $comment1->setPseudo('Pseudo2')
-        ->setEmail('email1@hotmail.fr')
+        $comment1->setEmail('email1@hotmail.fr')
         ->setContenu('Super event')
         ->setActif('1')
-        ->setCreatedAt(\DateTime::createFromFormat('d-m-Y',$date2));
+        ->setCreatedAt(\DateTime::createFromFormat('d-m-Y',$date2))
+        ->setEvent(1);
         $manager->persist($comment1);
 
         $comment2=new Commentaire();
         $date2 = "22-04-2022";
         $roles[]='ROLE_ADMIN';
-        $comment2->setPseudo('Pseudo2')
-        ->setEmail('email2@hotmail.fr')
+        $comment2->setEmail('email2@hotmail.fr')
         ->setContenu('Super event')
         ->setActif('0')
-        ->setCreatedAt(\DateTime::createFromFormat('d-m-Y',$date2));
+        ->setCreatedAt(\DateTime::createFromFormat('d-m-Y',$date2))
+        ->setEvent(1);
         $manager->persist($comment2);
 
         $comment3=new Commentaire();
         $date2 = "22-04-2022";
         $roles[]='ROLE_ADMIN';
-        $comment3->setPseudo('Pseudo3')
-        ->setEmail('email3@hotmail.fr')
+        $comment3->setEmail('email3@hotmail.fr')
         ->setContenu('Super event')
         ->setActif('1')
-        ->setCreatedAt(\DateTime::createFromFormat('d-m-Y',$date2));
+        ->setCreatedAt(\DateTime::createFromFormat('d-m-Y',$date2))
+        ->setEvent(2);
         $manager->persist($comment3);
 
         $manager->flush();
